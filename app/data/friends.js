836f8ab070fb9ab24879eaa39d5648aa1270a4e3
sweave19 <47548028+sweave19@users.noreply.github.com>
2019-07-24 23:09:34 -0500

    Add files via upload

---
+++ app/data/friends.js
@@ -0,0 +1,43 @@
+// Templated Friends Data
+var friendsData = [
+    {
+      name: "Goku",
+      photo: "https://vignette.wikia.nocookie.net/deathbattlefanon/images/2/20/C0A9B238-91F6-46AA-ABDC-3FC720228C25.png/revision/latest?cb=20171104014554",
+      scores: [3, 4, 3, 3, 4, 4, 4, 1, 5, 3]
+    },
+    {
+      name: "Ash",
+      photo:"https://upload.wikimedia.org/wikipedia/en/thumb/0/09/AshXYanime.png/220px-AshXYanime.png",
+      scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    },
+    {
+      name: "Deku",
+      photo:"https://vignette.wikia.nocookie.net/characterprofile/images/e/e1/Izuku_Midoriya%2C_the_Successor_to_All_Might.png/revision/latest?cb=20170801171151",
+      scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    {
+      name: "Naruto",
+      photo:"https://vignette.wikia.nocookie.net/youtubepoop/images/e/ea/Naruto.png/revision/latest?cb=20150422212156",
+      scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+    {
+      name: "Inuyasha",
+      photo:"http://4.bp.blogspot.com/__DFlANNL8ow/TG4cfcyGmcI/AAAAAAAAAAw/Gd51uuezPw0/s1600/inuyasha.jpg",
+      scores: [5, 4, 4, 4, 4, 3, 2, 1, 5, 5]
+    },
+    {
+      name: "Gon",
+      photo:"https://vignette.wikia.nocookie.net/hunterxhunter/images/b/b4/Gon-2011.png/revision/latest?cb=20120115022050",
+      scores: [2, 4, 2, 4, 2, 3, 4, 4, 4, 3]
+    },
+    {
+      name: "Natsu",
+      photo:"https://vignette.wikia.nocookie.net/p__/images/e/ea/Natsu_Dragneel.png/revision/latest?cb=20190427121605&path-prefix=protagonist",
+      scores: [5, 4, 5, 4, 5, 3, 3, 3, 2, 4]
+    }
+  ];
+  
+  
+  
+  // Export to API Route
+  module.exports = friendsData;
